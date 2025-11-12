edc27f410d729aea306d80615e84f8eef82fab80
ssawaul <ssawaul@gmail.com>
2025-11-12 19:57:21 +0900

    1112

--- ch10/app.py
+++ ch10/app.py
@@ -8,7 +8,7 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT']=0
 
 @app.route('/')
 def index():
-        return render_template('10-2.html')
+        return render_template('10.html')
 
 if __name__ == "__main__":
         app.run(host='0.0.0.0', port=8080, debug=True)
