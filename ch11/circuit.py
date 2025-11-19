e64293cb591ab525f13cd08780f6f405508e4af3
ssawaul <ssawaul@gmail.com>
2025-11-19 18:42:00 +0900

    1119

---
+++ ch11/circuit.py
@@ -0,0 +1,39 @@
+import time
+import RPi.GPIO as GPIO
+
+# LED를 켜고 끄는 함수
+def controlLED(on_off): # led 번호의 핀에 on_off(0/1) 값 출력하는 함수
+	global led
+	GPIO.output(led, on_off)
+
+# 초음파 센서를 제어하여 물체와의 거리를 측정하여 거리 값 리턴하는 함수
+def measure_distance():
+	global trig, echo
+	time.sleep(0.2) # 초음파 센서의 준비 시간을 위해 200밀리초 지연
+	GPIO.output(trig, 1) # trig 핀에 1(High) 출력
+	GPIO.output(trig, 0) # trig 핀에 0(Low) 출력. High->Low. 초음파 발사 지시
+
+	while(GPIO.input(echo) == 0): # echo 핀 값이 0->1로 바뀔 때까지 루프
+		pass
+
+	# echo 핀 값이 1이면 초음파가 발사되었음
+	pulse_start = time.time() # 초음파 발사 시간 기록
+	while(GPIO.input(echo) == 1): # echo 핀 값이 1->0으로 바뀔 때까지 루프
+		pass
+	
+	# echo 핀 값이 0이 되면 초음파 수신하였음
+	pulse_end = time.time() # 초음파가 되돌아 온 시간 기록
+	pulse_duration = pulse_end - pulse_start # 경과 시간 계산
+	return pulse_duration*340*100/2 # 거리 계산하여 리턴(단위 cm)
+
+# 초음파 센서를 다루기 위한 전역 변수 선언 및 초기화
+trig = 20 # GPIO20
+echo = 16 # GPIO16
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(trig, GPIO.OUT) # GPIO20 핀을 출력으로 지정
+GPIO.setup(echo, GPIO.IN) # GPIO16 핀을 입력으로 지정
+
+# LED를 다루기 위한 전역 변수 선언 및 초기화
+led = 6 # GPIO6
+GPIO.setup(led, GPIO.OUT) # GPIO6 핀을 출력으로 지정
---
+++ ch11/mqtt.py
@@ -0,0 +1,29 @@
+import time
+import paho.mqtt.client as mqtt
+import circuit 
+
+def on_connect(client, userdata, flag, rc, prop=None):
+	client.subscribe("led") # "led" 토픽으로 구독 신청
+
+def on_message(client, userdata, msg) :
+	on_off = int(msg.payload); # on_off는 0 또는 1의 정수
+	circuit.controlLED(on_off) # LED를 켜거나 끔
+
+ip = "localhost" # 현재 브로커는 이 컴퓨터에 설치되어 있음
+
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect(ip, 1883) # 브로커에 연결
+client.loop_start() # 메시지 루프를 실행하는 스레드 생성
+
+# 도착하는 메시지는 on_message() 함수에 의해 처리되어 LED를 켜거나 끄는 작업과
+# 병렬적으로 1초 단위로 초음파 센서로부터 거리를 읽어 전송하는 무한 루프 실행
+while True:
+	distance = circuit.measure_distance() # 초음파 센서로부터 거리 읽기
+	client.publish("ultrasonic", distance) # “ultrasonic” 토픽으로 거리 전송
+	time.sleep(1) # 1초 동안 잠자기
+
+client.loop_stop() # 메시지 루프를 실행하는 스레드 종료
+client.disconnect()
