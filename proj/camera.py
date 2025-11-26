e97720880e5d3f4504ac5dc01f0fa9a1139b6d92
ssawaul <ssawaul@gmail.com>
2025-11-26 19:28:14 +0900

    1126

---
+++ proj/camera.py
@@ -0,0 +1,24 @@
+import cv2
+def init():
+	global camera
+	# 카메라 객체를 생성하고 촬영한 사진 크기를 640x480으로 설정
+	camera = cv2.VideoCapture(0, cv2.CAP_V4L)
+	camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+	camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+	# 프레임을 임시 저장할 버퍼 개수를 1로 설정
+	buffer_size = 1
+	camera.set(cv2.CAP_PROP_BUFFERSIZE, buffer_size)
+def take_picture(most_recent=False):
+	global camera
+	len = 0 if most_recent == False else camera.get(cv2.CAP_PROP_BUFFERSIZE)
+	while(len > 0):
+		camera.grab()
+		len -=1
+		success, image = camera.read()
+		if not success:
+			return None
+		return image
+def final():
+	if camera != None:
+		camera.release()
+camera = None
--- proj/mqtt.py
+++ proj/mqtt.py
@@ -2,17 +2,8 @@ import time
 import paho.mqtt.client as mqtt
 import circuit 
 import RPi.GPIO as GPIO
-
 import cv2
-# 카메라 객체를 생성하고 촬영한 사진 크기를 640x480으로 설정
-camera = cv2.VideoCapture(0, cv2.CAP_V4L)
-camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-# 프레임을 임시 저장할 버퍼 개수를 1로 설정
-buffer_size = 1
-camera.set(cv2.CAP_PROP_BUFFERSIZE, buffer_size)
-
-
+import camera
 
 red_on = 0
 blue_on = 0
@@ -40,10 +31,9 @@ try:
 		client.publish("ultrasonic", distance) # “ultrasonic” 토픽으로 거리 전송
 		time.sleep(1) # 1초 동안 잠자기
 		if distance < 20 : # 물체와의 거리가 10cm 이내이면
-			for i in range(buffer_size+1): 
-				ret, frame = camera.read()
-				im_bytes = cv2.imencode('.jpg', frame)[1].tobytes() # 바이트 배열로 저장
-				cv2.imwrite('./data/picture.jpg', im_bytes)
+			image = camera.take_picture(most_recent=True)
+			if image is not None:
+				cv2.imwrite('./data/cctv.jpg', image)
 			if (red_on == 0) :
 				circuit.ledred_on()
 				red_on = 1
