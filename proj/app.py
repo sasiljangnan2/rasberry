e5abfa22facf9947f4e1d9ae1ce81d44763a772f
ssawaul <ssawaul@gmail.com>
2025-11-26 23:32:59 +0900

    1126

--- proj/app.py
+++ proj/app.py
@@ -8,6 +8,8 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT']=0
 @app.route('/')
 def index():
 	return render_template('11-4.html')
-
+@app.route('/cctv')
+def cctv():
+	return render_template('11-4.html',fname='/data/cctv.jpg')
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=8080, debug=True)
