6418d12ea5c87dae79ec119e69ac2828db8fefd4
ssawaul <ssawaul@gmail.com>
2025-11-28 21:01:46 +0900

    1128

--- proj/static/mqttio4.js
+++ proj/static/mqttio4.js
@@ -53,7 +53,7 @@ function publish(topic, msg) {
 }
 function publishid(topic, id)
 {
-	return publish(topic, document.getElementById(id));
+	return publish(topic, document.getElementById(id).textContent);
 }
 function unsubscribe(topic) {
 	if(connectionFlag != true) return; // 연결되지 않은 경우
