e3c1afd1e3b8c7ae00c1d6f0fabfa123f4064178
ssawaul <ssawaul@gmail.com>
2025-11-28 20:05:22 +0900

    11289

--- proj/circuit.py
+++ proj/circuit.py
@@ -11,6 +11,9 @@ is_Alert = 0
 def controlAlert(on_off): # led 번호의 핀에 on_off(0/1) 값 출력하는 함수
 	global is_Alert
 	is_Alert = on_off
+	file = open('./data/text.txt', 'w') # 추가 모드로 열
+	file.write(is_Alert)
+	file.close()
 def doAlert():
     global is_Alert
     return is_Alert
