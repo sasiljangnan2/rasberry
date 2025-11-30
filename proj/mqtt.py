3cece537c0642a5c4cf1d3efa9894471cc7d4581
ssawaul <ssawaul@gmail.com>
2025-11-30 17:32:50 +0900

    1130

--- proj/mqtt.py
+++ proj/mqtt.py
@@ -32,7 +32,7 @@ try:
 		distance = circuit.measure_distance() # 초음파 센서로부터 거리 읽기
 		client.publish("ultrasonic", distance) # “ultrasonic” 토픽으로 거리 전송
 		time.sleep(0.5) # 1초 동안 잠자기
-		if distance < 50: # 물체와의 거리가 50cm 이내이면
+		if distance < 20: # 물체와의 거리가 50cm 이내이면
 			nowtime = time.strftime('%Y-%m-%d%H:%M:%S') # 현재 시간 저장
 			file = open('./data/alert.txt', 'a') # 추가 모드로 열기
 			data = "%s,%s\n" % (nowtime, distance) # data에 date,dis 형식으로 저장
@@ -42,7 +42,7 @@ try:
 			if circuit.doAlert() == 1: # 경보 상태라면 led 반복 점등과 경보 소리 실행
 				circuit.repert_led()
 				playmp3.playsound()
-		else :
+		if circuit.doAlert() == 0 : # 경보 상태가 아니라면 
 			circuit.led_off()
 			playmp3.stopsound()
 except KeyboardInterrupt:
